b5ad4814e43d2e73b1f104bb724e14b8c5eb8dfa
Anton Sankov <asankov96@gmail.com>
2025-05-05 16:15:18 +0300

    Fix imports

--- pkg/vulns/vulns.go
+++ pkg/vulns/vulns.go
@@ -1,8 +1,8 @@
 package vulns
 
 import (
-	"github.com/castai/osv-scanner/v2/internal/imodels"
-	"github.com/castai/osv-scanner/v2/internal/utility/vulns"
+	"github.com/google/osv-scanner/v2/internal/imodels"
+	"github.com/google/osv-scanner/v2/internal/utility/vulns"
 	"github.com/ossf/osv-schema/bindings/go/osvschema"
 )
 
