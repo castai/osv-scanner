ab1dabfd6ea6411929748be5c4c102049b29ab62
Anton Sankov <asankov96@gmail.com>
2025-05-05 14:30:40 +0300

    Expose `PackageInfo` type

--- pkg/vulns/vulns.go
+++ pkg/vulns/vulns.go
@@ -1,11 +1,13 @@
 package vulns
 
 import (
-	"github.com/google/osv-scanner/v2/internal/imodels"
-	"github.com/google/osv-scanner/v2/internal/utility/vulns"
+	"github.com/castai/osv-scanner/v2/internal/imodels"
+	"github.com/castai/osv-scanner/v2/internal/utility/vulns"
 	"github.com/ossf/osv-schema/bindings/go/osvschema"
 )
 
-func IsAffected(v osvschema.Vulnerability, pkg imodels.PackageInfo) bool {
+type PackageInfo = imodels.PackageInfo
+
+func IsAffected(v osvschema.Vulnerability, pkg PackageInfo) bool {
 	return vulns.IsAffected(v, pkg)
 }
