f2e4e3f9d83014e197404151437dd27124e036b9
Anton Sankov <asankov96@gmail.com>
2025-05-05 12:36:57 +0300

    Expose `IsAffected`

--- pkg/comparer/comparer.go
+++ pkg/comparer/comparer.go
@@ -3,10 +3,17 @@ package comparer
 import (
 	"github.com/google/osv-scanner/internal/semantic"
 	"github.com/google/osv-scanner/pkg/models"
+	"github.com/google/osv-scanner/v2/internal/imodels"
+	"github.com/google/osv-scanner/v2/internal/utility/vulns"
+	"github.com/ossf/osv-schema/bindings/go/osvschema"
 )
 
 type Version = semantic.Version
 
 func Parse(str string, ecosystem models.Ecosystem) (Version, error) {
 	return semantic.Parse(str, ecosystem)
+}
+
+func IsAffected(v osvschema.Vulnerability, pkg imodels.PackageInfo) bool {
+	return vulns.IsAffected(v, pkg)
 }
